95a183e4275729ac32bf511e8cc766525d15d880
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-01-24 20:23:30 +0000

    Upto load balancers on pg72

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_launch_configuration" "example" {
     ami             = "ami-0fb653ca2d3203ac1"
     instance_type   = "t2.micro"
     vpc_security_group_ids = [aws_security_group.instance.id]
@@ -12,6 +12,10 @@ resource "aws_instance" "example" {
                 echo "Hello, World.. I am waking up" > index.html
                 nohup busybox httpd -f -p ${var.server_port} & 
                 EOF
+
+    lifecycle {
+        create_before_destroy = true
+    }
 
     user_data_replace_on_change = true
 
@@ -31,6 +35,20 @@ resource "aws_security_group" "instance" {
     }
 }
 
+resource "aws_autoscaling_group" "example" {
+    launch_configuration    = aws_launch_configuration.example.name
+    vpc_zone_identifier     = data.aws_subnets.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propogate_at_launch = true 
+    }
+}
+
 variable "server_port" {
     description = "The port the server will use for HTTP requests!"
     type        = number
@@ -40,4 +58,15 @@ variable "server_port" {
 output "public_ip" {
     value       = aws_instance.example.public_ip
     description = "The public IP address of the webserver"
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "default" {
+    filter {
+        name        = "vpc-id"
+        values      = [data.aws_vpc.default.id]
+    }
 }
